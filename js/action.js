6dbd86ff98dcade3a21667d775422a35d942404e
Sasha Ye <akomat1103@gmail.com>
2019-11-13 15:34:17 +0800

    first-commit header ok

---
+++ js/action.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+	$('.user-toggle').click(function(e) {
+		e.preventDefault();
+		e.stopPropagation();
+
+		// if($('#nav').length>0) {
+		// 	$('#nav').removeClass().addClass('.account');
+		// }
+		
+		$('.account').fadeToggle();
+	});
+
+	$('.menu-toggle').click(function(e) {
+		e.preventDefault();
+		e.stopPropagation();
+
+		// if($('.account').length>0) {
+		// 	$('.account').removeClass().addClass('#nav');
+		// }
+		
+		$('#nav').fadeToggle();
+	});
+});
